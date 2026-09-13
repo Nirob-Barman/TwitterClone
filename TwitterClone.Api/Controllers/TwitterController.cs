56064a2a103339945b593a817d68a81ab5194e90
Nirob-Barman <niroborin111@gmail.com>
2026-09-13 18:36:40 +0600

    feature/twitter-api-webapi

---
+++ TwitterClone.Api/Controllers/TwitterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterClone.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwitterController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TwitterController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet("tweets")]
+        public IActionResult GetTweets()
+        {
+            var maxTweetLength = _configuration.GetValue<int>("TwitterSettings:MaxTweetLength");
+
+            var response = new
+            {
+                maxLenth = maxTweetLength,
+                tweets = new[]
+                {
+                    new { Id = 1, Text = "Exploring Web API development with ASP.NET Core" },
+                    new { Id = 2, Text = "Learning how to build RESTful APIs using C#" }
+
+                }
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("app-info")]
+        public IActionResult GetAppInfo()
+        {
+            var appName = _configuration.GetValue<string>("AppName");
+            return Ok(new { AppName = appName });
+        }
+
+    }
+}
---
+++ TwitterClone.Api/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterClone.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new { Id = 1, Name = "Bill Gates", Handle = "@billgates" },
+                new { Id = 2, Name = "Elon Musk", Handle = "@elonmusk" },
+                new { Id = 3, Name = "Mark Zuckerberg", Handle = "@zuck" },
+                new { Id = 4, Name = "Larry Page", Handle = "@larrypage" },
+                new { Id = 5, Name = "Sundar Pichai", Handle = "@sundarpichai" },
+            };
+
+            return Ok(users);
+        }
+    }
+}
